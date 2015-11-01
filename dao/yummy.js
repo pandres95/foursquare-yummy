1fb4302056ce5d89d3e34f5c4dc57750a5e5bc99
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-11-01 17:10:10 -0500

    Just return the Venue

--- dao/yummy.js
+++ dao/yummy.js
@@ -8,7 +8,7 @@ module.exports = function(app){
     this.find = function (lat, lng, radius) {
         return places.explore(lat, lng, radius || 1000).then((data) => {
             var places = data.groups[0].items;
-            return places[random.randomInt(0, places.length)];
+            return places[random.randomInt(0, places.length)].venue;
         });
     };
 
